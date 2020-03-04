efa7f1539206300542461ea6099e47b5f8f2d0ac
jbultsma <joshuabultsma@gmail.com>
2020-03-04 18:55:23 -0500

    Initial Commit

---
+++ 3and5s/3and5s/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3and5s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            List<int> numbers = new List<int>();
+
+            for (int i=0; i<1000; i++)
+            {               
+                if(i%3==0 || i%5==0)
+                {
+                    numbers.Add(i);                    
+                }
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
